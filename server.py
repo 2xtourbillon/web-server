36e3ca0f653268d75c1dc452e85aea4b510d0649
Gurgen Verdian <gurgen.verdian@gmail.com>
2021-05-01 19:09:59 -0400

    adding additional updates

--- server.py
+++ server.py
@@ -4,16 +4,13 @@ print(__name__)
 
 @app.route('/')
 def my_home():
-    return render_template('./templates/index.html')
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('./templates/about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/blog')
-def blog():
-    return 'These on my thoughts on blogs'
-    
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'This is my dog'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submitted!'
+
